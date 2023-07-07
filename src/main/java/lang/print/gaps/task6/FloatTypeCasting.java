2476b9c1f0502b76200499a9c72a0b728e056c2a
Ainur <ayazbay.ainur@mail.ru>
2023-07-07 23:34:46 +0600

    1st commit

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        System.out.println(Math.round(numberToBeRounded));
+    }
+
+    public static void main(String[] args){
+        var classInstance  = new FloatTypeCasting();
+        classInstance.roundNumber(12.5f);
     }
 }
