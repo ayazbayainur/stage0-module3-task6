e730df0cacf985953c1f2799ac02e85adba13d2e
Ainur <ayazbay.ainur@mail.ru>
2023-07-07 23:48:04 +0600

    2nd commit

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -3,8 +3,8 @@ package lang.print.gaps.task6;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first = 1000;
-        int second = 10;
-        first = second;
+        int second = (int) first;
+//        first = second;
         System.out.println(first);
         System.out.println(second);
     }
